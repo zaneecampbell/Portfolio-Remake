ea2c975739da40281ba7d7efea8ecff98f5608dd
Zane Campbell <zanegitinit@gmail.com>
2022-08-11 11:48:53 -0700

    Updates

---
+++ precache-manifest.aacff82368d79e6a544ffb17ddf3aa89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a75ac047a9665e1b287c41f4733ff66",
+    "url": "/Portfolio-Remake/index.html"
+  },
+  {
+    "revision": "4aa2f5734fe012f8bccc",
+    "url": "/Portfolio-Remake/static/css/main.9ee2e248.chunk.css"
+  },
+  {
+    "revision": "53c92fbddd89b089c333",
+    "url": "/Portfolio-Remake/static/js/2.27e6fff5.chunk.js"
+  },
+  {
+    "revision": "4aa2f5734fe012f8bccc",
+    "url": "/Portfolio-Remake/static/js/main.b5f0ca16.chunk.js"
+  },
+  {
+    "revision": "8308d6f54a444bd721a4",
+    "url": "/Portfolio-Remake/static/js/runtime~main.a2f537d1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Portfolio-Remake/precache-manifest.aacff82368d79e6a544ffb17ddf3aa89.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-Remake/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
